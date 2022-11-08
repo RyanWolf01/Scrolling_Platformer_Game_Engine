ec211353354daa2ced1e0991b0e6b380fddd0f8a
Andrew Demma <andrew.demma@duke.edu>
2022-11-08 01:13:16 -0500

    Finish planning for Andy

--- doc/plan/TEST_PLAN.md
+++ doc/plan/TEST_PLAN.md
@@ -6,9 +6,9 @@ that implements Alive actually causes the lives counter to go to 0.
 * Test the functionality of the LifeIncrementer. For example, see if an entity that implements
   LifeIncrementer can increase the lives of the entity. 
 ### Andy
-Scenario 1
-Scenario 2
-Scenario 3
+* Test if the Camera actually moves when the player gets outside the movable box. I will test the camera location after a movement.
+* Test if an enemy actually disappears once its model counterpart has died.
+* Test how the camera reacts if Mario is suddenly moved (teleported) to outside the playable area. What will the camera do at this moment?
 ### Manith
 * Try to add duplicate entries to EntityContainer
 * Make sure character Y_coordinate drops when not on platform
--- doc/plan/api/view/Camera.java
+++ doc/plan/api/view/Camera.java
@@ -1,12 +1,30 @@
-
+/**
+ * The Camera will handle the operation of deciding when and where the viewable area of the game will scroll when the player moves.
+ * It will also hold margin values that allow the player to move freely inside of a box of the viewable area before scrolling.
+ * Lastly, the game can control moments when the camera does not scroll (could be end/beginning of level or cutscenes).
+ */
 
 
 public interface Camera {
 
 
+/**
+* Updates where the player is currently, and does operations to scroll
+ * @param playerLocation
+*/
   public void setPlayerLocation(Point2D.Double playerLocation);
 
+/**
+* Gets the camera origin for purposes of showing/ not showing things in the camera area.
+ * @return the origin point of the camera (top-left)
+*/
   public Point2D.Double getCameraLocation();
+/**
+* Stops the ability to scroll
+*/
   public void stopScroll();
+/**
+* Although scrolling is automatically enabled at the start of the class, this method is used to restart scrolling.
+*/
   public void startScroll();
 }
