16607d3a9a7991b63072ae7eff337232c68a11e8
Ryan Wolfram <ryan.wolfram@duke.edu>
2022-11-08 00:39:51 -0500

    added documentation for collision_handling apis

--- doc/plan/api/model/collision_handling/CollisionChartLoader.java
+++ doc/plan/api/model/collision_handling/CollisionChartLoader.java
@@ -1,3 +1,6 @@
+/**
+ * Loads the CollisionChart object from a JSON file for entityType passed as a parameter.
+ */
 public interface CollisionChartLoader {
   CollisionChart loadCollisionChart(String entityType);
 }
