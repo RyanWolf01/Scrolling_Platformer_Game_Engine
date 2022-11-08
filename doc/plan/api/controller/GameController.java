81571edab884a644770536676eb5ef7490c5b2d5
Arman Shekarriz <abs78@duke.edu>
2022-11-08 02:00:40 -0500

    updating UPI files

--- doc/plan/api/controller/GameController.java
+++ doc/plan/api/controller/GameController.java
@@ -4,7 +4,7 @@
  * program and the model of the program. It will contain methods and functionality to
  * communicate changes to the view from the model that need to be reflected to the user on the
  * UI that appears. In addition, it provides functionality to adjust the model and change locations
- * of the main character and other
+ * of the main character/player, this interface and API will cooperate with the LevelDataWrapper API.
  */
 
 public interface GameController {
