06e1836e40855f957044fe462729eb105617f157
Arman Shekarriz <abs78@duke.edu>
2022-11-08 01:35:14 -0500

    adding APIs JSONTranslator and GameController

---
+++ doc/plan/api/controller/GameController.java
@@ -0,0 +1,14 @@
+/**
+ * API OVERVIEW DOCUMENT
+ * This API will serve as the brain of the program and the connection between the view of the
+ * program and the model of the program. It will contain methods and functionality to
+ * communicate changes to the view from the model that need to be reflected to the user on the
+ * UI that appears. In addition, it provides functionality to adjust the model and change locations
+ * of the main character and other
+ */
+
+public interface GameController {
+
+  public void
+
+}
