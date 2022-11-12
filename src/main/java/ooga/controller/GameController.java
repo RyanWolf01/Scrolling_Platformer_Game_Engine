d4f0308e9e6df02db890e9e3ffddd55a45054dc5
Manith Luthria <mnl21@duke.edu>
2022-11-12 14:38:32 -0500

    issue #23 resolved

---
+++ src/main/java/ooga/controller/GameController.java
@@ -0,0 +1,28 @@
+package ooga.controller;
+
+import ooga.model.entities.Entity;
+import ooga.model.entities.containers.ImmutableContainer;
+
+/**
+ * This is how the View and model will be able to communicate. This class
+ * will be running during the gameplay.
+ */
+public class GameController {
+    private ImmutableContainer entities;
+
+    public GameController(){
+
+    }
+
+    public void step(){
+
+    }
+
+    public void handleKeyInput(){
+
+    }
+
+    public void handleCollisions(Entity collider, Entity collided){
+
+    }
+}
