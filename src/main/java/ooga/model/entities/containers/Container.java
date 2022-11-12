c5b20ffbef15c2dd92672e82b7cae437ff352239
Michael Habib <michael.habib@duke.edu>
2022-11-12 12:33:55 -0500

    making Container abstract class. working on #3

---
+++ src/main/java/ooga/model/entities/containers/Container.java
@@ -0,0 +1,44 @@
+package ooga.model.entities.containers;
+
+import java.util.ArrayList;
+import java.util.List;
+import ooga.model.entities.Entity;
+import ooga.model.entities.ImmutableEntity;
+
+public abstract class Container {
+
+  private List<Entity> entities;
+
+  /**
+   * default constructor
+   */
+  public Container(){
+    entities = new ArrayList<>();
+  }
+
+  /**
+   * If you want to add an entity upon creation of the container
+   * @param entity
+   */
+  public Container(Entity entity){
+    entities = new ArrayList<>();
+    entities.add(entity);
+  }
+
+  /**
+   * Add Entity
+   */
+  public void addEntity(Entity entity){
+    entities.add(entity);
+  }
+
+  /**
+   * Get ImmutableEntity at index
+   * @param index index in container
+   * @return ImmutableEntity
+   */
+  protected ImmutableEntity getEntity(int index){
+    return getEntity(index);
+  }
+
+}
