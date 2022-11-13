59815698d9a2d4ab796628c1846ae426444c725d
Michael Habib <michael.habib@duke.edu>
2022-11-13 16:39:07 -0500

    Commenting MainCharacter class

--- src/main/java/ooga/model/entities/characters/MainCharacter.java
+++ src/main/java/ooga/model/entities/characters/MainCharacter.java
@@ -9,18 +9,32 @@ import ooga.model.entities.movement.Mover;
 public abstract class MainCharacter extends Entity implements Alive, Mover {
     private int lives;
 
+    /**
+     * Constructor for MainCharacter class that calls superclass Entity constructor
+     * @param attributes initial conditions (x, y positions)
+     * @param entityInfo information specific to subclass
+     * @param length length of Mario (in y direction)
+     * @param width width of Mario (in x direction)
+     * @param lives initial lives
+     */
     public MainCharacter(InitialAttributes attributes, Info entityInfo, double length, double width, int lives) {
         super(attributes, entityInfo, length, width);
         this.lives = lives;
     }
 
-
+    /**
+     * Returns number of lives of the current entity is alive. Extended by other
+     * interfaces.
+     */
     @Override
     public int getLives() {
         return lives;
     }
 
-
+    /**
+     * This method should perform all actions necessary to kill the entity. This is specific to a given entity, but
+     * for Mario this may include setting its velocities to 0 and disabling abilities.
+     */
     @Override
     public void kill() {
         lives--;
