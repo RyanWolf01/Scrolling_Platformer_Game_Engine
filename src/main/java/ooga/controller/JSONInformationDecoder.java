5fbd9afb4d0c544c3c0fd59f2967c96d3802915c
Arman Shekarriz <abs78@duke.edu>
2022-11-13 11:29:43 -0500

    resolved issue #25, working on issue #7

--- src/main/java/ooga/controller/JSONInformationDecoder.java
+++ src/main/java/ooga/controller/JSONInformationDecoder.java
@@ -1,12 +1,28 @@
 package ooga.controller;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
+import org.json.simple.parser.*;
 
 public class JSONInformationDecoder implements JSONTranslator {
 
-  public JSONObject initialJSONInformation(String JSONFilePath) throws FileNotFoundException {
-
+  /**
+   * This method takes in the JSONFilePath as a string from the file reading where this clqss lives
+   * and converts the JSON file chosen by the user into a JSON object of initial game information
+   * needed by the model and view
+   * @param JSONFilePath, string for the filepath to the JSON file selected by the user
+   * @return initialGameStates, JSONObject containing information about the game
+   * @throws IOException
+   * @throws ParseException
+   */
+  public JSONObject initialJSONInformation(String JSONFilePath) throws IOException, ParseException {
+    FileReader infoFile = new FileReader(JSONFilePath);
+    JSONObject initialGameStates = (JSONObject) new JSONParser().parse(infoFile);
+    return initialGameStates;
   }
+
+
 
 }
--- src/main/java/ooga/controller/JSONTranslator.java
+++ src/main/java/ooga/controller/JSONTranslator.java
@@ -1,9 +1,11 @@
 package ooga.controller;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 
 public interface JSONTranslator {
 
-  public JSONObject initialJSONInformation(String JSONFilePath) throws FileNotFoundException;
+  public JSONObject initialJSONInformation(String JSONFilePath) throws IOException, ParseException;
 }
