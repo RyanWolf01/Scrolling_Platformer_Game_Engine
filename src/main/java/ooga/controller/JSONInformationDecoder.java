3f615b3abf701915028551919b9a52eeb71ac7ea
Arman Shekarriz <abs78@duke.edu>
2022-11-13 12:51:32 -0500

    working on isue #7, container factory implementation for entity container

--- src/main/java/ooga/controller/JSONInformationDecoder.java
+++ src/main/java/ooga/controller/JSONInformationDecoder.java
@@ -1,10 +1,13 @@
 package ooga.controller;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import ooga.model.entities.containers.EntityContainer;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
-import org.json.simple.parser.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ContainerFactory;
+import org.json.simple.parser.ParseException;
+
 
 public class JSONInformationDecoder implements JSONTranslator {
 
@@ -23,6 +26,10 @@ public class JSONInformationDecoder implements JSONTranslator {
     return initialGameStates;
   }
 
+  public EntityContainer makeEntityContainerFromJSONObject(JSONObject initialGameJSON) throws IOException, ParseException {
+    EntityContainer entities = new EntityContainer();
 
+
+  }
 
 }
--- src/main/java/ooga/controller/JSONTranslator.java
+++ src/main/java/ooga/controller/JSONTranslator.java
@@ -2,10 +2,14 @@ package ooga.controller;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import ooga.model.entities.containers.EntityContainer;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
 public interface JSONTranslator {
 
   public JSONObject initialJSONInformation(String JSONFilePath) throws IOException, ParseException;
+
+  public EntityContainer makeEntityContainerFromJSONObject(JSONObject initialGameJSON) throws IOException, ParseException;
+
 }
