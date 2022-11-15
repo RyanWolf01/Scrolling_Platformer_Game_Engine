f66dafd61f4e50539309c91c9373ddf3f2ad676a
Michael Habib <michael.habib@duke.edu>
2022-11-15 01:38:30 -0500

    Testing Kill. close #41

--- src/test/java/ooga/model/actions/aliveactions/KillTest.java
+++ src/test/java/ooga/model/actions/aliveactions/KillTest.java
@@ -1,5 +1,59 @@
 package ooga.model.actions.aliveactions;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import ooga.model.entities.EntityInfo;
+import ooga.model.entities.characters.Mario;
+import org.junit.jupiter.api.Test;
+
 public class KillTest {
 
+  @Test
+  void testKillPos1() {
+    Mario mario = new Mario(0, 0, 2, 2, null);
+
+    IncreaseLife increaseLife = new IncreaseLife();
+    increaseLife.execute(mario);
+    increaseLife.execute(mario);
+    increaseLife.execute(mario);
+
+    Kill kill = new Kill();
+    kill.execute(mario);
+
+    assertEquals(2, mario.getLives());
+  }
+
+  /**
+   * execute twice
+   */
+  @Test
+  void testKillPos2() {
+    Mario mario = new Mario(0, 0, 2, 2, null);
+
+    IncreaseLife increaseLife = new IncreaseLife();
+    increaseLife.execute(mario);
+    increaseLife.execute(mario);
+    increaseLife.execute(mario);
+
+    Kill kill = new Kill();
+    kill.execute(mario);
+    kill.execute(mario);
+
+    assertEquals(1, mario.getLives());
+  }
+
+  /**
+   * don't execute
+   */
+  @Test
+  void testKillNeg() {
+    Mario mario = new Mario(0, 0, 2, 2, null);
+
+    Kill kill = new Kill();
+
+    assertEquals(0, mario.getLives());
+  }
+
 }
+
+
