6d47fbee5aec92e2757c220ee7edcbf3714acd2c
Arman Shekarriz <abs78@duke.edu>
2022-11-15 03:14:04 -0500

    some testing stuff and fixing errors, note error in game controller test resulting from refactoring in JSONDecoding, to be fixed

--- src/test/java/ooga/controller/tests/JSONDecodingTest.java
+++ src/test/java/ooga/controller/tests/JSONDecodingTest.java
@@ -1,5 +1,6 @@
 package ooga.controller.tests;
 
+
 import org.junit.jupiter.api.Test;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
@@ -7,6 +8,15 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class JSONDecodingTest {
+
+  // make some sample JSON array and object here to use to test methods
+  JSONArray sampleArray = new JSONArray();
+
+  JSONObject sampleEntity1 = new JSONObject();
+  JSONObject sampleEntity2 = new JSONObject();
+  JSONObject sampleEntity3 = new JSONObject();
+
+
 
   @Test
   void testJSONObjectCreation() {
