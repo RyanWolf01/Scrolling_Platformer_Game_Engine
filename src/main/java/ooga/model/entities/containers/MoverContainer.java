986e6c056601d1a5e95648f48a578efc388247f7
Michael Habib <michael.habib@duke.edu>
2022-11-15 02:11:40 -0500

    Tested MoverContainer and refactored the class as well. close #45

--- src/main/java/ooga/model/entities/containers/MoverContainer.java
+++ src/main/java/ooga/model/entities/containers/MoverContainer.java
@@ -1,38 +1,62 @@
 package ooga.model.entities.containers;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 import ooga.model.entities.Entity;
+import ooga.model.entities.ImmutableEntity;
 import ooga.model.entities.movement.Mover;
 
-public class MoverContainer extends EntityContainer {
+public class MoverContainer {
 
+  private List<Mover> movers;
   /**
    * default constructor
    */
   public MoverContainer(){
-    super();
+    movers = new ArrayList<>();
   }
 
   /**
    * If you want to add an entity upon creation of the container
-   * @param entity
+   * @param mover
    */
-  public MoverContainer(Entity entity){
-    super(entity);
+  public MoverContainer(Mover mover){
+    movers = new ArrayList<>();
+    movers.add(mover);
+  }
+
+  /**
+   * Add Mover
+   */
+  public void addMover(Mover mover){
+    movers.add(mover);
+  }
+
+  /**
+   * Get ImmutableEntity at index
+   * @param index index in container
+   * @return ImmutableEntity
+   */
+  public Mover getMover(int index){
+    return movers.get(index);
+  }
+
+  /**
+   *
+   * @return size of container
+   */
+  public int getContainerSize(){
+    return movers.size();
   }
 
   /**
    * call move method on all Movers
    */
   public void moveAll(){
-
-    try {
-      for (int index = 0; index < getContainerSize(); index++) {
-        Mover currMover = (Mover) getEntity(index);
-        currMover.move();
-      }
-    }
-    catch(ClassCastException exception){
-      throw exception;
+    for (int index = 0; index < getContainerSize(); index++) {
+      Mover currMover = getMover(index);
+      currMover.move();
     }
   }
 
