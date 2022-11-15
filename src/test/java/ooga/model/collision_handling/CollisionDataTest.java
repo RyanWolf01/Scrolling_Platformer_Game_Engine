423a7b56064778b7bfea8fd8f98259712e7690ba
Ryan Wolfram <ryan.wolfram@duke.edu>
2022-11-14 22:26:18 -0500

    added test for CollisionData

---
+++ src/test/java/ooga/model/collision_handling/CollisionDataTest.java
@@ -0,0 +1,44 @@
+package ooga.model.collision_handling;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import ooga.model.Info;
+import ooga.model.collisions.CollisionPhysicsInfo;
+import ooga.model.collisions.collision_handling.CollisionData;
+import ooga.model.collisions.data.CollisionDirection;
+import ooga.model.entities.EntityInfo;
+import org.junit.jupiter.api.Test;
+
+public class CollisionDataTest {
+
+  @Test
+  public void testCollisionData() {
+    EntityInfo entityAInfo = new EntityInfo("GOOMBA");
+    EntityInfo entityBInfo = new EntityInfo("MARIO");
+    CollisionPhysicsInfo cpi = new CollisionPhysicsInfo(true, CollisionDirection.LEFT);
+
+    String[] kvPairsA = new String[]{"powerUp", "NONE", "test", "FOO"};
+    String[] kvPairsB = new String[]{"powerUp", "STAR", "BTest", "BAR"};
+    String[] cpiKvPairs = new String[]{"substance", "WATER"};
+
+    setInfo(entityAInfo, kvPairsA);
+    setInfo(entityBInfo, kvPairsB);
+    setInfo(cpi, cpiKvPairs);
+
+    CollisionData collisionData = new CollisionData(entityAInfo, entityBInfo, cpi);
+    String[] combined = new String[]{"MY_powerUp", "NONE", "MY_test", "FOO", "OPPONENT_powerUp", "STAR", "OPPONENT_BTest", "BAR", "COLLISION_substance", "WATER"};
+
+    for (int i = 0; i < combined.length; i += 2) {
+      assertTrue(cpi.hasKey(combined[i]));
+      assertEquals(cpi.get(combined[i]), combined[i + 1]);
+    }
+  }
+
+  private void setInfo(Info info, String[] data) {
+    for (int i = 0; i < data.length; i += 2) {
+      info.set(data[i], data[i+1]);
+    }
+  }
+
+}
