3cd0dab17017d74f3d3527d753fe162f5627a55e
Michael Habib <michael.habib@duke.edu>
2022-11-19 12:07:55 -0500

    Tested new classes. #59

---
+++ src/test/java/ooga/model/actions/moveractions/basicmovement/DownwardMovementTest.java
@@ -0,0 +1,50 @@
+package ooga.model.actions.moveractions.basicmovement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import ooga.model.actions.moveractions.StopXMovement;
+import ooga.model.entities.characters.Mario;
+import org.junit.jupiter.api.Test;
+
+public class DownwardMovementTest {
+
+  @Test
+  void testMovementPos1() {
+    Mario mario = new Mario(0, 15, 2, 2, null);
+
+    DownwardMovement move = new DownwardMovement();
+    move.execute(mario);
+
+    assertEquals(0, mario.getXCoordinate());
+    assertEquals(10, mario.getYCoordinate());
+  }
+
+  /**
+   * stop twice
+   */
+  @Test
+  void testMovementPos2() {
+    Mario mario = new Mario(0, 15, 2, 2, null);
+
+    DownwardMovement move = new DownwardMovement();
+    move.execute(mario);
+    move.execute(mario);
+
+    assertEquals(0, mario.getXCoordinate());
+    assertEquals(0, mario.getYCoordinate());
+  }
+
+  /**
+   * don't execute
+   */
+  @Test
+  void testMovementNeg() {
+    Mario mario = new Mario(0, 0, 2, 2, null);
+
+    DownwardMovement move = new DownwardMovement();
+
+    assertEquals(0, mario.getXCoordinate());
+    assertEquals(0, mario.getYCoordinate());
+  }
+
+}
