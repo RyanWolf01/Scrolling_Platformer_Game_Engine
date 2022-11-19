47093df339004b3b968955de153047cd6aacb960
Michael Habib <michael.habib@duke.edu>
2022-11-19 11:44:11 -0500

    Created basic movement classes but need to make sure they work. #59

---
+++ src/main/java/ooga/model/actions/moveractions/basicmovement/LeftMovement.java
@@ -0,0 +1,20 @@
+package ooga.model.actions.moveractions.basicmovement;
+
+import ooga.model.actions.moveractions.MoverAction;
+import ooga.model.entities.movement.Mover;
+
+public class LeftMovement implements MoverAction {
+
+  private static final int LEFT_VELOCITY = -5; // change this to properties file
+
+  /**
+   * moves entity up in y direction by a fixed amount
+   * @param entity
+   */
+  @Override
+  public void execute(Mover entity){
+    entity.changeVelocities(LEFT_VELOCITY, 0);
+    entity.move();
+  }
+
+}
