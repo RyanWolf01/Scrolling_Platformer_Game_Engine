bb1b933767204ac2a2e0c412e9d78268c50c2168
Michael Habib <michael.habib@duke.edu>
2022-11-20 18:08:18 -0500

    Implemented concrete class that extends StaticCharacter so I can test it. #67

---
+++ src/main/java/ooga/model/entities/characters/BasicStaticCharacter.java
@@ -0,0 +1,46 @@
+package ooga.model.entities.characters;
+
+import ooga.model.entities.data.Info;
+import ooga.model.actionparsers.AliveActionParser;
+import ooga.model.actions.aliveactions.AliveAction;
+import ooga.model.collisions.data.ActionDataContainer;
+import ooga.model.entities.movement.MovementQueue;
+
+public class BasicStaticCharacter extends StaticCharacter {
+
+  /**
+   * AutomaticMovingCharacter has lives and can move; moves automatically
+   * @param initialXCoordinate
+   * @param initialYCoordinate
+   * @param height
+   * @param width
+   * @param entityInfo
+   */
+  public BasicStaticCharacter(int initialXCoordinate, int initialYCoordinate, double height, double width,
+      Info entityInfo, MovementQueue movementQueue) {
+    super(initialXCoordinate, initialYCoordinate, height, width, entityInfo);
+  }
+
+  /**
+   * Implements method in Alive interface that changes object's lives
+   *
+   * @param changeInLives is the change in lives
+   */
+  @Override
+  public void changeLives(int changeInLives) {
+    setLives(getLives() + changeInLives);
+  }
+
+  @Override
+  public void performActions(ActionDataContainer actionDataContainer) {
+    performAliveAction(actionDataContainer);
+  }
+  private void performAliveAction(ActionDataContainer actionDataContainer) {
+    AliveActionParser aliveActionParser = new AliveActionParser(actionDataContainer);
+    if (aliveActionParser.hasAction()) {
+      AliveAction aliveAction = aliveActionParser.getAction();
+      aliveAction.execute(this);
+    }
+  }
+
+}
