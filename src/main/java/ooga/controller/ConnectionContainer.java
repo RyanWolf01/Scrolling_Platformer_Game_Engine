9bb2350eb50871b13b63d281cd88fcb6c26de4f1
mnl21 <mnl21@duke.edu>
2022-11-21 20:15:30 -0500

    resolved #79, made backend container

--- src/main/java/ooga/controller/ConnectionContainer.java
+++ src/main/java/ooga/controller/ConnectionContainer.java
@@ -11,6 +11,7 @@ import ooga.model.entities.CollidableEntity;
 import ooga.model.entities.Entity;
 import ooga.model.entities.characters.maincharacters.MainCharacterEntity;
 import ooga.model.entities.containers.AutomaticMoverContainer;
+import ooga.model.entities.containers.BackendContainer;
 import ooga.model.entities.containers.CollidableContainer;
 import ooga.model.entities.data.EntityInfo;
 import ooga.model.entities.containers.EntityContainer;
@@ -26,17 +27,12 @@ import ooga.view.nodes.ScrollingNode;
 public class ConnectionContainer {
   public static final ResourceBundle entityClassResources = ResourceBundle.getBundle(Main.DEFAULT_RESOURCE_PACKAGE+"Entities");
   public static final ResourceBundle containerResources = ResourceBundle.getBundle(Main.DEFAULT_RESOURCE_PACKAGE+"Containers");
-  private EntityContainer entities;
-  private AutomaticMoverContainer autoMovers;
-  private CollidableContainer collidables;
-  private MainCharacterEntity mainCharacter;
+  private BackendContainer entities;
   private NodeContainer nodes;
   private Map<ScrollingNode, Entity> connectorMap;
 
   public ConnectionContainer(){
-    entities = new EntityContainer();
-    autoMovers = new AutomaticMoverContainer();
-    collidables = new CollidableContainer();
+    entities = new BackendContainer();
     nodes = new NodeContainer();
     connectorMap = new HashMap<>();
   }
@@ -52,58 +48,6 @@ public class ConnectionContainer {
    * @param info extra info that might be helpful
    */
   public void addNewEntity(int xCoordinate, int yCoordinate, double height, double width, String type, EntityInfo info){
-    Entity newEntity = null;
-    if(isMainCharacter(type)){ // if it's a main character type entity, overwrite the basic newEntity
-      MainCharacterEntity main;
-      try {
-        main = (MainCharacterEntity) Class.forName(entityClassResources.getString(type)).
-            getConstructor(Integer.class, Integer.class, Double.class, Double.class, EntityInfo.class)
-            .newInstance(xCoordinate,yCoordinate, height, width, info);
-      } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException |
-               InstantiationException | IllegalAccessException e) {
-        throw new RuntimeException(e);
-      }
-      mainCharacter = main;
-      newEntity = main;
-      collidables.addCollidable(main); // all main characters are collidable
-    }
-    else if(isAutomaticMover(type)){
-      // TODO: fix this so that there is a movement queue, not just null
-
-      AutomaticMovingEntity newMover;
-      try {
-        newMover = (AutomaticMovingEntity) Class.forName(entityClassResources.getString(type)).
-            getConstructor(Integer.class, Integer.class, Double.class, Double.class, EntityInfo.class, MovementQueue.class)
-            .newInstance(xCoordinate,yCoordinate, height, width, info, null);
-      } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException |
-               InstantiationException | IllegalAccessException e) {
-        throw new RuntimeException(e);
-      }
-
-      autoMovers.addMover(newMover);
-      newEntity = newMover;
-
-      if(isCollidable(type)){
-        collidables.addCollidable(newMover);
-      }
-    }
-    else if(isCollidable(type)){ // only a collidable
-      CollidableEntity newCollidable;
-
-      try {
-        newCollidable = (CollidableEntity) Class.forName(entityClassResources.getString(type)).
-            getConstructor(Integer.class, Integer.class, Double.class, Double.class, EntityInfo.class)
-            .newInstance(xCoordinate,yCoordinate, height, width, info);
-      } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException |
-               InstantiationException | IllegalAccessException e) {
-        throw new RuntimeException(e);
-      }
-
-      collidables.addCollidable(newCollidable);
-      newEntity = newCollidable;
-    }
-
-
     String imageURL = null;
     try{
       imageURL = info.get("texture");
@@ -113,12 +57,12 @@ public class ConnectionContainer {
       //imageURL = something from default_images resource file
     }
 
-    ScrollingNode node = new ScrollingNode(xCoordinate, yCoordinate, height, width, imageURL);
+    Entity newEntity = entities.addNewEntity(xCoordinate, yCoordinate, height, width, type, info);
+    ScrollingNode newNode = new ScrollingNode(xCoordinate, yCoordinate, height, width, imageURL);
 
-    entities.addEntity(newEntity);
-    nodes.addNode(node);
+    nodes.addNode(newNode);
 
-    connectorMap.put(node, newEntity);
+    connectorMap.put(newNode, newEntity);
   }
 
   /**
@@ -147,17 +91,4 @@ public class ConnectionContainer {
       node.update(entity.getXCoordinate(), entity.getYCoordinate());
     }
   }
-
-  private boolean isMainCharacter(String type){
-    return Arrays.asList(containerResources.getStringArray("main_characters")).contains(type);
-  }
-
-  private boolean isAutomaticMover(String type){
-    return Arrays.asList(containerResources.getStringArray("automatic_movers")).contains(type);
-  }
-
-  private boolean isCollidable(String type){
-    return Arrays.asList(containerResources.getStringArray("collidables")).contains(type);
-  }
-
 }
