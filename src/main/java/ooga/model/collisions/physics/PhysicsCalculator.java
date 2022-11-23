f1cb85651cd5cf74c3d3b38ed1ea9a2f682098ae
Ryan Wolfram <ryan.wolfram@duke.edu>
2022-11-23 15:15:37 -0500

    wrote methods in Edge within PhysicsCalculator to check collisions between edges of rectangles
    Former-commit-id: 5e1f3f94cae2a6f9e7dfc7816afdd8e71363febc

--- src/main/java/ooga/model/collisions/physics/PhysicsCalculator.java
+++ src/main/java/ooga/model/collisions/physics/PhysicsCalculator.java
@@ -1,6 +1,5 @@
 package ooga.model.collisions.physics;
 
-import ooga.model.entities.CollidableEntity;
 import ooga.model.entities.Entity;
 
 public class PhysicsCalculator {
@@ -67,5 +66,109 @@ public class PhysicsCalculator {
     return null;
   }
 
+  private CollisionDirection checkDirection2(Entity a, Entity b) {
+    return null;
+  }
+
+  private class Coordinate {
+    double x, y;
+    public Coordinate(double x, double y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+  private class Edge {
+    CollisionDirection direction;
+    Coordinate vertex1;
+    Coordinate vertex2;
+    double yVelocity;
+    double xVelocity;
+
+    public Edge(CollisionDirection side, Coordinate vertex1, Coordinate vertex2, double xVelocity, double yVelocity) {
+      if ((vertex1.y == vertex2.y) && (side == CollisionDirection.TOP || side == CollisionDirection.BOTTOM)) {
+        direction = side;
+      }
+      else if ((vertex1.x == vertex2.x) && (side == CollisionDirection.RIGHT || side == CollisionDirection.LEFT)) {
+        direction = side;
+      }
+      else {
+        throw new RuntimeException("Improper usage of Edge");
+      }
+
+      this.vertex1 = vertex1;
+      this.vertex2 = vertex2;
+      this.xVelocity = xVelocity;
+      this.yVelocity = yVelocity;
+
+    }
+
+    public boolean willIntersect(Edge otherEdge) {
+      if (!bothHorizontalOrVertical(this, otherEdge)) {
+        return false;
+      }
+
+      if (this.direction == CollisionDirection.TOP || this.direction == CollisionDirection.BOTTOM) {
+        return willIntersectTopOrBottom(otherEdge);
+      }
+      else {
+        return willIntersectLeftOrRight(otherEdge);
+      }
+    }
+
+    // TODO: consolidate willIntersectTopOrBottom and willIntersectLeftOrRight into one method
+    private boolean willIntersectTopOrBottom(Edge otherEdge) {
+      // Calculate time for x values to intersect
+      double denominator = (otherEdge.yVelocity - this.yVelocity);
+      if (denominator == 0) {
+        return false;
+      }
+      double time = (this.vertex1.y - otherEdge.vertex1.y) / denominator;
+
+      // get the x values at this time
+      double thisX1 = this.vertex1.x + this.xVelocity * time;
+      double thisX2 = this.vertex2.x + this.xVelocity * time;
+      double otherX1 = otherEdge.vertex1.x + otherEdge.xVelocity * time;
+      double otherX2 = otherEdge.vertex2.x + otherEdge.xVelocity * time;
+
+      // make sure the x values are in range of each other
+      return (thisX2 >= otherX1 && thisX1 <= otherX2);
+    }
+
+    private boolean willIntersectLeftOrRight(Edge otherEdge) {
+      // Calculate time for x values to intersect
+      double denominator = (otherEdge.xVelocity - this.xVelocity);
+      if (denominator == 0) {
+        return false;
+      }
+      double time = (this.vertex1.x - otherEdge.vertex1.x) / denominator;
+
+      // get the x values at this time
+      double thisY1 = this.vertex1.y + this.yVelocity * time;
+      double thisY2 = this.vertex2.y + this.yVelocity * time;
+      double otherY1 = otherEdge.vertex1.y + otherEdge.yVelocity * time;
+      double otherY2 = otherEdge.vertex2.y + otherEdge.yVelocity * time;
+
+      // make sure the x values are in range of each other
+      return (thisY2 >= otherY1 && thisY1 <= otherY2);
+    }
+
+    private boolean bothHorizontalOrVertical(Edge edge1, Edge edge2) {
+      if (edge1.direction == CollisionDirection.TOP) {
+        return edge2.direction == CollisionDirection.TOP || edge2.direction == CollisionDirection.BOTTOM;
+      }
+      if (edge1.direction == CollisionDirection.BOTTOM) {
+        return edge2.direction == CollisionDirection.TOP || edge2.direction == CollisionDirection.BOTTOM;
+      }
+      if (edge1.direction == CollisionDirection.RIGHT) {
+        return edge2.direction == CollisionDirection.RIGHT || edge2.direction == CollisionDirection.LEFT;
+      }
+      if (edge1.direction == CollisionDirection.LEFT) {
+        return edge2.direction == CollisionDirection.RIGHT || edge2.direction == CollisionDirection.LEFT;
+      }
+
+      return false;
+    }
+
+  }
 
 }
