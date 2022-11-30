442ebd3496ce03a5f06d52d4f3e4a70885b4e018
Ryan Wolfram <ryan.wolfram@duke.edu>
2022-11-29 20:18:19 -0500

    added logging to Exceptions
    Former-commit-id: 1b91fba450a9943a5f0d44db5d896e7c60e8238c

--- src/main/java/ooga/model/actionparsers/ActionParsingException.java
+++ src/main/java/ooga/model/actionparsers/ActionParsingException.java
@@ -1,16 +1,23 @@
 package ooga.model.actionparsers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Exception thrown when an exception occurs while running (or as a result of running) the
  * ActionParsers.
  */
 public class ActionParsingException extends RuntimeException {
 
+  private static final Logger LOG = LogManager.getLogger(ActionParsingException.class);
+
   public ActionParsingException(String s) {
     super(s);
+    LOG.error(s);
   }
 
   public ActionParsingException(String s, Exception e) {
     super(s, e);
+    LOG.error(s, e);
   }
 }
--- src/main/java/ooga/model/collisions/collisionhandling/exceptions/NoCollisionCriteriaMatchException.java
+++ src/main/java/ooga/model/collisions/collisionhandling/exceptions/NoCollisionCriteriaMatchException.java
@@ -1,11 +1,16 @@
 package ooga.model.collisions.collisionhandling.exceptions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * An exception thrown when no criteria in a CollisionChart can be matched for a given collision
  */
 public class NoCollisionCriteriaMatchException extends RuntimeException {
+  private static final Logger LOG = LogManager.getLogger(NoCollisionCriteriaMatchException.class);
 
   public NoCollisionCriteriaMatchException(String message) {
     super(message);
+    LOG.error(message);
   }
 }
