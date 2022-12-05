40d31dfda98ed2459f4e92e991b3ef85f9a72408
Arman Shekarriz <abs78@duke.edu>
2022-12-05 14:48:03 -0500

    adding icons, adding button classes and properties file with string pathes to icons, abstract button class
    Former-commit-id: 3ef6747b52436b08328bbe62d4a4436d989b6bfc

--- src/main/java/ooga/view/levelscreenbuttons/GUIBasicButton.java
+++ src/main/java/ooga/view/levelscreenbuttons/GUIBasicButton.java
@@ -16,8 +16,41 @@ import java.util.ResourceBundle;
 
 public abstract class GUIBasicButton {
 
+  public static final String GUI_ICON_PROPERTIES = "properties.GUIButtons";
+  public static final ResourceBundle ICON_PROPERTIES = ResourceBundle.getBundle(GUI_ICON_PROPERTIES);
+
   private Button button;
   private Text buttonText;
   private VBox buttonContainer;
+
+
+
+
+  public GUIBasicButton(String buttonText, String iconID) {
+
+    // load the graphic using resources
+    ResourceBundle iconResources = ResourceBundle.getBundle(GUI.GUI_ICON_PROPERTIES);
+    InputStream iconPath = GUIButton.class.getClassLoader().getResourceAsStream(
+        iconResources.getString(iconID));
+    buttonContainer = new VBox();
+
+    // set icon graphic appearances
+    if (iconPath == null) {
+      throw new NullPointerException("Invalid button icon path");
+    }
+    icon = new ImageView(new Image(iconPath));
+    icon.setFitWidth(properties.getButtonProperty("defaultGraphicWidth"));
+    icon.setFitHeight(properties.getButtonProperty("defaultGraphicHeight"));
+
+    // set up button
+    button = new Button();
+    button.setGraphic(icon);
+
+    this.buttonText = new Text(buttonText);
+    this.buttonText.setFont(Font.font("Helvetica", FontWeight.BOLD, 14));
+    buttonContainer.getChildren().addAll(button, this.buttonText);
+    buttonContainer.setAlignment(Pos.CENTER);
+  }
+
 
 }
