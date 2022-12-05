494ee261bddcb78d703361823d92e51c778f495e
Arman Shekarriz <abs78@duke.edu>
2022-12-05 13:39:40 -0500

    refactoring of checkpointDirectory class
    Former-commit-id: b292762902cde5f84fe12b4e1075f588069414a1

--- src/main/java/ooga/controller/saveloadhandling/CheckpointDirectory.java
+++ src/main/java/ooga/controller/saveloadhandling/CheckpointDirectory.java
@@ -14,9 +14,17 @@ public class CheckpointDirectory {
   private JSONObject levelJSON;
 
 
-
   // link used here below, on writing json objects to files
   // https://crunchify.com/how-to-write-json-object-to-file-in-java/
+
+  /**
+   * Default constructor for a CheckpointDirectory, which is what will be saved in resources when a user presses save game
+   * and then inputs what they want the directory to be called, which is the name it will be saved as
+   * @param directoryName
+   * @param level
+   * @param controls
+   * @param collision
+   */
   public CheckpointDirectory(String directoryName, JSONObject level, JSONObject controls, JSONObject collision) {
     this.savedDirectoryName = directoryName;
     this.levelJSON = level;
@@ -24,6 +32,11 @@ public class CheckpointDirectory {
     this.collisionsJSON = collision;
   }
 
+  /**
+   * Method to create the directory once one decides they want to save the game or load the game
+   * Used to actually make the new directory and add the JSON files that save the game state to this directory
+   * in the savedgames directory within resources
+   */
   public void CreateDirectory() {
     new File(RESOURCE_DIRECTORY+SAVED_GAMES+savedDirectoryName).mkdirs();
     try {
