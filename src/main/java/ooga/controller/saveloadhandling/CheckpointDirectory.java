7f862b689f1a6a18f7741b259a39e6376fd4a8b8
Arman Shekarriz <abs78@duke.edu>
2022-12-05 02:42:48 -0500

    default constructor for a CheckpointDirectory made
    Former-commit-id: 54616ff68e9cfaade01672387c0d7bdf6107d91d

--- src/main/java/ooga/controller/saveloadhandling/CheckpointDirectory.java
+++ src/main/java/ooga/controller/saveloadhandling/CheckpointDirectory.java
@@ -1,13 +1,17 @@
 package ooga.controller.saveloadhandling;
 
 import java.io.File;
+import org.json.simple.JSONObject;
 
 public class CheckpointDirectory {
   private static final String RESOURCE_DIRECTORY = "/";
   private static final String SAVED_GAMES = "savedgames/";
+  private JSONObject collisionsJSON;
+  private JSONObject controlsJSON;
+  private JSONObject levelJSON;
 
 
-  public CheckpointDirectory(String directoryName, ) {
+  public CheckpointDirectory(String directoryName, JSONObject level, JSONObject controls, JSONObject collision) {
     new File(RESOURCE_DIRECTORY+SAVED_GAMES+directoryName).mkdirs();
 
   }
