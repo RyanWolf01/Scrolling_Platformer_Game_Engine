3b9bdad89a921af94b3a34b5431af98b1eead879
Arman Shekarriz <abs78@duke.edu>
2022-12-05 16:01:29 -0500

    abstract class for button complete, close issue #111
    Former-commit-id: 796391e7161dc2da50a29790c6d40de976ca14a6

--- src/main/java/ooga/view/levelscreenbuttons/SaveButton.java
+++ src/main/java/ooga/view/levelscreenbuttons/SaveButton.java
@@ -1,6 +1,10 @@
 package ooga.view.levelscreenbuttons;
 
+import javafx.stage.Stage;
+
 public class SaveButton extends GUIBasicButton {
+
+  private Stage stage;
 
   public SaveButton(String buttonText, String iconString) {
     super(buttonText, iconString);
