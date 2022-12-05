270f8143eb723777976c8f13ee4b9002b0b9c40e
Arman Shekarriz <abs78@duke.edu>
2022-12-05 12:06:07 -0500

    updating methodology to save a checkpoint directory to saved games
    Former-commit-id: cea98e17b8042dc386abd10e49bdfcff615f0bb7

--- src/main/java/ooga/controller/saveloadhandling/CheckpointDirectory.java
+++ src/main/java/ooga/controller/saveloadhandling/CheckpointDirectory.java
@@ -8,27 +8,36 @@ import org.json.simple.JSONObject;
 public class CheckpointDirectory {
   private static final String RESOURCE_DIRECTORY = "/";
   private static final String SAVED_GAMES = "savedgames/";
+  private String savedDirectoryName;
   private JSONObject collisionsJSON;
   private JSONObject controlsJSON;
   private JSONObject levelJSON;
 
 
+
   // link used here below, on writing json objects to files
   // https://crunchify.com/how-to-write-json-object-to-file-in-java/
   public CheckpointDirectory(String directoryName, JSONObject level, JSONObject controls, JSONObject collision) {
-    new File(RESOURCE_DIRECTORY+SAVED_GAMES+directoryName).mkdirs();
+    this.savedDirectoryName = directoryName;
+    this.levelJSON = level;
+    this.controlsJSON = controls;
+    this.collisionsJSON = collision;
+  }
+
+  public void CreateDirectory() {
+    new File(RESOURCE_DIRECTORY+SAVED_GAMES+savedDirectoryName).mkdirs();
     try {
-      FileWriter levelFile = new FileWriter(RESOURCE_DIRECTORY+SAVED_GAMES+directoryName+"/level.json");
-      levelFile.write(level.toJSONString());
-      FileWriter controlFile = new FileWriter(RESOURCE_DIRECTORY+SAVED_GAMES+directoryName+"/controls.json");
-      controlFile.write(level.toJSONString());
-      FileWriter collisionFile = new FileWriter(RESOURCE_DIRECTORY+SAVED_GAMES+directoryName+"/collision.json");
-      collisionFile.write(level.toJSONString());
+      FileWriter levelFile = new FileWriter(RESOURCE_DIRECTORY+SAVED_GAMES+savedDirectoryName+"/level.json");
+      levelFile.write(levelJSON.toJSONString());
+      FileWriter controlFile = new FileWriter(RESOURCE_DIRECTORY+SAVED_GAMES+savedDirectoryName+"/controls.json");
+      controlFile.write(controlsJSON.toJSONString());
+      FileWriter collisionFile = new FileWriter(RESOURCE_DIRECTORY+SAVED_GAMES+savedDirectoryName+"/collision.json");
+      collisionFile.write(collisionsJSON.toJSONString());
 
     } catch (IOException e) {
+      // TODO: handle exception and log
       throw new RuntimeException(e);
     }
   }
-
 
 }
