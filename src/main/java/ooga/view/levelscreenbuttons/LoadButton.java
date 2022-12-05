e23c72109dbf05362d73c8425b9fc128de8f8d6e
Arman Shekarriz <abs78@duke.edu>
2022-12-05 16:04:30 -0500

    close issue #109, load button implemented
    Former-commit-id: 933255a5846ca7e375077f97958d8b25090878db

--- src/main/java/ooga/view/levelscreenbuttons/LoadButton.java
+++ src/main/java/ooga/view/levelscreenbuttons/LoadButton.java
@@ -31,7 +31,7 @@ public class LoadButton extends GUIBasicButton {
   private void openDirectoryChooser(ActionEvent e) {
     // TODO: need this method to perform opening with right stage (game stage)
     File levelDirectory = directoryChooser.showDialog(stage);
-    // System.out.println(levelDirectory);
+    System.out.println(levelDirectory);
     // LOG.debug(this.levelDirectory);
   }
 }
