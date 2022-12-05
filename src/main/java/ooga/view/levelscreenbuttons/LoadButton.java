5c98004f868413d0c367ad27699a3a3dfc898d17
Arman Shekarriz <abs78@duke.edu>
2022-12-05 15:20:30 -0500

    implementation of directory chooser for the load button to load saved games
    Former-commit-id: dada4cafa6ea977ebe3708c1fb9dc8d5b6553802

--- src/main/java/ooga/view/levelscreenbuttons/LoadButton.java
+++ src/main/java/ooga/view/levelscreenbuttons/LoadButton.java
@@ -1,9 +1,20 @@
 package ooga.view.levelscreenbuttons;
 
+import java.io.File;
+import javafx.stage.DirectoryChooser;
+
 public class LoadButton extends GUIBasicButton {
 
+  public static final String SAVED_GAME_PATH = "savedgames/";
+  private DirectoryChooser directoryChooser;
   public LoadButton(String buttonText, String iconString) {
     super(buttonText, iconString);
 
+    directoryChooser = new DirectoryChooser();
+    directoryChooser.setInitialDirectory(new File(SAVED_GAME_PATH));
+    // TODO: need this method to
+    // File levelDirectory = directoryChooser.showDialog(NEED RIGHT STAGE HERE);
+    // System.out.println(levelDirectory);
+    // LOG.debug(this.levelDirectory);
   }
 }
