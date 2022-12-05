d4404c34807ea364bac86939101bd3524c313cda
Arman Shekarriz <abs78@duke.edu>
2022-12-05 02:49:08 -0500

    finished default constructor and writing to files for checkpoint direc
    Former-commit-id: 32b39fe17aa63b08a0c29ba5f4c90bbadc5314fd

--- src/main/java/ooga/controller/saveloadhandling/CheckpointDirectory.java
+++ src/main/java/ooga/controller/saveloadhandling/CheckpointDirectory.java
@@ -1,6 +1,8 @@
 package ooga.controller.saveloadhandling;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import org.json.simple.JSONObject;
 
 public class CheckpointDirectory {
@@ -11,9 +13,21 @@ public class CheckpointDirectory {
   private JSONObject levelJSON;
 
 
+  // link used here below, on writing json objects to files
+  // https://crunchify.com/how-to-write-json-object-to-file-in-java/
   public CheckpointDirectory(String directoryName, JSONObject level, JSONObject controls, JSONObject collision) {
     new File(RESOURCE_DIRECTORY+SAVED_GAMES+directoryName).mkdirs();
+    try {
+      FileWriter levelFile = new FileWriter(RESOURCE_DIRECTORY+SAVED_GAMES+directoryName+"/level.json");
+      levelFile.write(level.toJSONString());
+      FileWriter controlFile = new FileWriter(RESOURCE_DIRECTORY+SAVED_GAMES+directoryName+"/controls.json")
+      controlFile.write(level.toJSONString());
+      FileWriter collisionFile = new FileWriter(RESOURCE_DIRECTORY+SAVED_GAMES+directoryName+"/collision.json");
+      collisionFile.write(level.toJSONString());
 
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
 
