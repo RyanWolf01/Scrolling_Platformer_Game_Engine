e2b8f8de3f6522264c239e0d0962103ae77012b9
Ryan Wolfram <ryan.wolfram@duke.edu>
2022-12-08 20:47:07 -0500

    refactored GameEnderStaticEntity to GameEnderCollidableEntity
    Former-commit-id: 5d5da7a9c6b6fc408c151e1939a83d4f6a05df64

---
+++ src/main/java/ooga/model/entities/modelcallers/GameEnderCollidableEntity.java
@@ -0,0 +1,35 @@
+package ooga.model.entities.modelcallers;
+
+import ooga.model.actionparsers.EndGameActionParser;
+import ooga.model.collisions.actiondata.ActionDataContainer;
+import ooga.model.collisions.collisionhandling.CollisionChart;
+import ooga.model.entities.collidable.CollidableEntity;
+import ooga.model.entities.info.Info;
+import ooga.model.entities.modelcallers.functionalinterfaces.EndGameCallable;
+
+public class GameEnderCollidableEntity extends CollidableEntity implements GameEnder {
+  EndGameCallable endGameMethod;
+
+  public GameEnderCollidableEntity(CollisionChart collisionChart, int initialXCoordinate, int initialYCoordinate, double height, double width,
+      Info entityInfo, EndGameCallable endGameMethod) {
+    super(collisionChart, initialXCoordinate, initialYCoordinate, height, width, entityInfo);
+    this.endGameMethod = endGameMethod;
+  }
+
+  @Override
+  public void endGame() {
+    if (endGameMethod == null) throw new RuntimeException("The end game method for mario hasn't"
+        + "been set!");
+    endGameMethod.execute();
+  }
+
+  @Override
+  public void setEndGameCallable(EndGameCallable endGameCallable) {
+    endGameMethod = endGameCallable;
+  }
+
+  @Override
+  protected int performActions(ActionDataContainer actionDataContainer) {
+    return new EndGameActionParser(actionDataContainer).parseAndApplyActions(this);
+  }
+}
