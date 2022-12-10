93566f64ea43b332399bf0543da410160406eebc
Michael Habib <michael.habib@duke.edu>
2022-12-10 15:59:08 -0500

    Refactoring AliveBehavior. #135
    Former-commit-id: 8d911d34c4cc978eb9a26f902e9f6eb4fdfdf883

---
+++ src/main/java/ooga/model/entities/livingentities/ImmutableAliveBehavior.java
@@ -0,0 +1,10 @@
+package ooga.model.entities.livingentities;
+
+public interface ImmutableAliveBehavior {
+
+  /**
+   * Returns number of lives of the current entity is alive.
+   */
+  int getLives();
+
+}
