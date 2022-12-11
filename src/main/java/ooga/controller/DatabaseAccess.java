89e2ca793a7bc2e3343e820934c6d9543ae20585
mnl21 <mnl21@duke.edu>
2022-12-11 18:28:07 -0500

    resolved #144, made online database
    Former-commit-id: ffc1de192721e12758f761d2c18c0b838c3ffc14

--- src/main/java/ooga/controller/DatabaseAccess.java
+++ src/main/java/ooga/controller/DatabaseAccess.java
@@ -1,5 +1,101 @@
 package ooga.controller;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Iterator;
+import org.json.JSONObject;
+
 public class DatabaseAccess {
+  public static final String DATABASE_URL = "https://scrolling-plagtform-default-rtdb.firebaseio.com/";
+  String game;
+
+  public DatabaseAccess(String game){
+    this.game = game;
+  }
+
+  private JSONObject getHighScores(){
+    HttpURLConnection con = null;
+    JSONObject result = null;
+
+    try {
+      con = (HttpURLConnection) new URL(DATABASE_URL+"high_scores/"+game+".json").openConnection();
+      con.setRequestMethod("GET");
+      con.setRequestProperty("Content-Type", "application/json");
+
+      BufferedReader in = new BufferedReader(
+          new InputStreamReader(con.getInputStream()));
+      String inputLine;
+      StringBuffer content = new StringBuffer();
+      while ((inputLine = in.readLine()) != null) {
+        content.append(inputLine);
+      }
+      in.close();
+
+      String received = content.toString();
+
+      result = new JSONObject(received);
+      System.out.println(result);
+
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    return result;
+  }
+
+  public void postHighScore(String username, int score){
+    JSONObject high_scores = getHighScores();
+
+    high_scores.put(username, score);
+
+    HttpURLConnection con = null;
+    try {
+      con = (HttpURLConnection) new URL(DATABASE_URL+"high_scores/"+game+".json").openConnection();
+      con.setRequestMethod("PUT");
+      con.setDoOutput(true);
+      con.setRequestProperty("Content-Type", "application/json");
+
+      OutputStreamWriter out = new OutputStreamWriter(
+          con.getOutputStream());
+      out.write(high_scores.toString());
+      out.close();
+
+      con.getInputStream();
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public String highestScorer(){
+    JSONObject json = getHighScores();
+
+    String highestScorer = null;
+    int highestScore = 0;
+
+    for (Iterator<String> it = json.keys(); it.hasNext(); ) {
+      String key = it.next();
+      int score = (int) json.get(key);
+
+      if(score > highestScore){
+        highestScorer = key;
+        highestScore = score;
+      }
+    }
+
+    return highestScorer;
+  }
+
+  public int highestScore(){
+    JSONObject json = getHighScores();
+
+    return (int) json.get(this.highestScorer());
+  }
 
 }
--- src/main/java/ooga/controller/UserControlHandler.java
+++ src/main/java/ooga/controller/UserControlHandler.java
@@ -66,21 +66,4 @@ public class UserControlHandler {
         return aliveActionMap.get(code);
     }
 
-//    private void setUpActions(){
-//        allAliveActions = new HashMap<>();
-//        allMoverActions = new HashMap<>();
-//
-//        allAliveActions.put("kill", new Kill());
-//        allAliveActions.put("increase_life", new IncreaseLife());
-//
-//        allMoverActions.put("bounce", new Bounce());
-//        allMoverActions.put("stop_both_directions", new StopBothDirections());
-//        allMoverActions.put("stop_x_movement", new StopXMovement());
-//        allMoverActions.put("stop_y_movement", new StopYMovement());
-//        allMoverActions.put("move_left", new LeftMovement());
-//        allMoverActions.put("move_right", new RightMovement());
-//        allMoverActions.put("move_down", new DownwardMovement());
-//        allMoverActions.put("move_up", new UpwardMovement());
-//    }
-
 }
