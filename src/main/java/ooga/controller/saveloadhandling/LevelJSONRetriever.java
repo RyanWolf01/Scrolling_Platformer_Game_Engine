65ac0a012bf9b49ba182e1fd0b7a6824360aa9c7
Arman Shekarriz <abs78@duke.edu>
2022-12-11 03:02:40 -0500

    working on issue #139, saving
    Former-commit-id: aaa54ab9db6357cc50755be3fe36adcd187de141

--- src/main/java/ooga/controller/saveloadhandling/LevelJSONRetriever.java
+++ src/main/java/ooga/controller/saveloadhandling/LevelJSONRetriever.java
@@ -1,17 +1,38 @@
 package ooga.controller.saveloadhandling;
 
+import ooga.model.entities.containers.BackendContainer;
+import ooga.model.entities.containers.LivingContainer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONObject;
 
 public class LevelJSONRetriever {
 
+  Logger LOG = LogManager.getLogger(LevelJSONRetriever.class);
+
+  public static final String ENTITY_KEY = "Entity";
   /*
   The purpose of this class is to take the backend container, or alive container,
   and be able to remake the level JSON from it to create a checkpoint directory
    */
-  public LevelJSONRetriever( ) {
 
+  /**
+   * Format of the level JSON:
+   * General, information
+   * Information about the entity that will need to be updated is the
+   * location (x and y) along with whether or not its alive
+   * EntityInfo stuff like lives may also change, so update this
+   * Entity: [{entity}, {entity}, {entity}]
+   */
+
+  JSONObject currentLevelJSON;
+
+
+  public LevelJSONRetriever(BackendContainer backendContainer, LivingContainer livingContainer) {
+    this.currentLevelJSON = generateLevelJSON();
   }
-  public JSONObject updatedLevelJSON() {
+
+  public JSONObject generateLevelJSON() {
     return new JSONObject();
   }
 
