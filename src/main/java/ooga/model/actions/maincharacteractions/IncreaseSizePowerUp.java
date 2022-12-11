34ad6ee5f9e68123f6f861c2decdafeb917b9757
Michael Habib <michael.habib@duke.edu>
2022-12-11 15:39:27 -0500

    Making increase size power up and new type of action. #133
    Former-commit-id: fae3b2cc01a546fa566b0935b886c1cde064a35b

---
+++ src/main/java/ooga/model/actions/maincharacteractions/IncreaseSizePowerUp.java
@@ -0,0 +1,33 @@
+package ooga.model.actions.maincharacteractions;
+
+import java.util.ResourceBundle;
+import ooga.controller.exceptions.MiscellaneousPropertiesException;
+import ooga.controller.exceptions.MovementDataException;
+import ooga.model.entities.entitymodels.MainCharacter;
+
+public class IncreaseSizePowerUp implements MainCharacterAction{
+
+  private final double SIZE_MULTIPLIER;
+  public IncreaseSizePowerUp(){
+    double tempMultiplier;
+    try{
+      tempMultiplier = Double.parseDouble(
+          ResourceBundle.getBundle("properties/powerups").getString("size_multiplier"));
+    }
+    catch(NumberFormatException exception){
+      throw new MiscellaneousPropertiesException("incorrect multiplier format", exception);
+    }
+    SIZE_MULTIPLIER = tempMultiplier;
+  }
+
+  /**
+   * executes action on entity
+   * @param entity on which action is executing
+   */
+  @Override
+  public void execute(MainCharacter entity){
+    entity.setHeight(entity.getHeight() * SIZE_MULTIPLIER);
+    entity.setWidth(entity.getWidth() * SIZE_MULTIPLIER);
+  }
+
+}
--- src/main/java/ooga/model/actions/moveractions/MoverAction.java
+++ src/main/java/ooga/model/actions/moveractions/MoverAction.java
@@ -2,7 +2,15 @@ package ooga.model.actions.moveractions;
 
 import ooga.model.entities.movement.Mover;
 
+/**
+ * action that executes on a moving entity
+ */
 public interface MoverAction {
+
+  /**
+   * executes on a moving entity
+   * @param entity on which action is executing
+   */
   void execute(Mover entity);
 
 }
