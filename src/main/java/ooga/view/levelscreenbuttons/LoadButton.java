495942b00eb2a09c611dc224a6811dd567ddf934
Arman Shekarriz <abs78@duke.edu>
2022-12-12 16:27:15 -0500

    fixing error with directory path for loading game
    Former-commit-id: 3e2ac59f4cb268c844caab754098535d3dd1e324

--- src/main/java/ooga/view/levelscreenbuttons/LoadButton.java
+++ src/main/java/ooga/view/levelscreenbuttons/LoadButton.java
@@ -1,5 +1,7 @@
 package ooga.view.levelscreenbuttons;
 
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import java.io.File;
 import javafx.stage.DirectoryChooser;
@@ -8,7 +10,10 @@ import ooga.controller.GameController;
 import ooga.view.View;
 
 public class LoadButton extends GUIBasicButton {
-  public static final String SAVED_GAME_PATH = "savedgames/";
+  public static final String SLASH = System.getProperty("file.separator");
+  public static final String SAVED_GAME_PATH = System.getProperty("user.dir") + SLASH + "src" + SLASH + "main" + SLASH
+      + "resources" + SLASH + "savedgames";
+
   private DirectoryChooser directoryChooser;
   private Stage stage;
 
