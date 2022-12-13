fc266e4bdb6f66484628dcfbd288e0a841b1645c
Arman Shekarriz <abs78@duke.edu>
2022-12-13 02:08:09 -0500

    making CHeckpoint directory tests
    Former-commit-id: 37220f748212dc576f9e40b569064384d08f4a42

--- src/test/java/ooga/controller/CheckpointDirectoryTest.java
+++ src/test/java/ooga/controller/CheckpointDirectoryTest.java
@@ -1,5 +1,13 @@
 package ooga.controller;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CheckpointDirectoryTest {
+
+  @Test
+  void testDirectoryCreation() {
+
+  }
 
 }
