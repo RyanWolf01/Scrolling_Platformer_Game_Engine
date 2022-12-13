7861f5f10c7f83a13e436b1ccb36cc0d97f82679
Ryan Wolfram <ryan.wolfram@duke.edu>
2022-12-13 02:31:35 -0500

    implemented IncrementScoreStandardTest
    Former-commit-id: 4e83c168c9fb81c8c2f1d9e808654da271e76020

--- src/test/java/ooga/model/actions/maincharacteractions/IncrementScoreStandardTest.java
+++ src/test/java/ooga/model/actions/maincharacteractions/IncrementScoreStandardTest.java
@@ -8,14 +8,14 @@ import ooga.model.entities.info.EntityInfo;
 import ooga.model.entities.movement.MovementQueue;
 import org.junit.jupiter.api.Test;
 
-public class IncrementScoreEnemyKilledTest {
+public class IncrementScoreStandardTest {
 
   @Test
   void testExecute() {
     BasicMainCharacter mainCharacter = new BasicMainCharacter(new DefaultCollisionChart(), 0, 0, 10, 10, new EntityInfo("Mario"), new MovementQueue());
     int score1 = mainCharacter.getScore();
-    new IncrementScoreEnemyKilled().execute(mainCharacter);
+    new IncrementScoreStandard().execute(mainCharacter);
 
-    assertEquals(score1 + 5, mainCharacter.getScore());
+    assertEquals(score1 + 1, mainCharacter.getScore());
   }
 }
