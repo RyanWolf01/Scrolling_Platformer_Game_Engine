98887e1e0d39f06bfe31c89c3fc05b4cb83f1ba6
Andrew Demma <andrew.demma@duke.edu>
2022-12-12 20:49:09 -0500

    #92 create title box and other language support for high scores
    Former-commit-id: 2a3343d7bca0a64af917f0ea09a8966e7325581d

--- src/main/java/ooga/view/screens/EndScreen.java
+++ src/main/java/ooga/view/screens/EndScreen.java
@@ -4,8 +4,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import ooga.Main;
 import ooga.view.nodes.HighScoreBox;
 import org.apache.logging.log4j.LogManager;
@@ -34,6 +35,7 @@ public class EndScreen {
   private HashMap<Integer, String> scoresMap = new HashMap();
   private static final Logger LOG = LogManager.getLogger(EndScreen.class);
   private ResourceBundle languageResources;
+  private HBox titleBox;
 
   /**
    * Creates an EndScreen To Display High Scores
@@ -50,6 +52,7 @@ public class EndScreen {
     levelPane.setId("Pane");
     levelPane.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
     initiateScoreFile(scoresPath);
+    createTitleBox();
     createTexts();
 
     Scene scene = new Scene(levelPane, sceneWidth, sceneHeight);
@@ -63,7 +66,7 @@ public class EndScreen {
     int boxHeight = (sceneHeight - titleHeight)/MAX_SCORES;
     for (int i = 0; i < Math.min(scoresMap.size(), MAX_SCORES); ++i) {
       HighScoreBox box = new HighScoreBox(sceneWidth, boxHeight, scoreList.get(i), scoresMap.get(scoreList.get(i)));
-      levelPane.add(box, 0, i);
+      levelPane.add(box, 0, i+1);
     }
   }
 
@@ -83,6 +86,7 @@ public class EndScreen {
       throw new RuntimeException(e);
     }
     JSONArray scores = (JSONArray) scoresJson.get("scores");
+    scores.add(createPlayerScoreObject());
     for (int i = 0; i < scores.size(); ++i) {
       JSONObject rec = (JSONObject) scores.get(i);
       String name = (String) rec.get("player");
@@ -91,11 +95,15 @@ public class EndScreen {
     }
   }
 
-  private File createNewScoresFile(String scoresPath){
+  private JSONObject createPlayerScoreObject() {
     JSONObject singleScore = new JSONObject();
-    // TODO: Get Player Score From Controller and Player Name
     singleScore.put("player", playerName);
     singleScore.put("score", playerScore);
+    return singleScore;
+  }
+
+  private File createNewScoresFile(String scoresPath){
+    JSONObject singleScore = createPlayerScoreObject();
     JSONArray arr = new JSONArray();
     arr.add(singleScore);
     JSONObject newScoresObject = new JSONObject();
@@ -112,5 +120,14 @@ public class EndScreen {
       e.printStackTrace();
     }
     return new File(this.getClass().getClassLoader().getResource(scoresPath).getFile());
+  }
+
+  private void createTitleBox(){
+    titleBox = new HBox();
+    titleBox.setPrefWidth(sceneWidth);
+    titleBox.setPrefHeight(titleHeight);
+    Text highScoreTitle = new Text(languageResources.getString("high_scores"));
+    titleBox.getChildren().add(highScoreTitle);
+    levelPane.add(titleBox, 0, 0);
   }
 }
