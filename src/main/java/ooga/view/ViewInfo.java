d161c7220bd62448eccc1c7b01da771511bf1a52
Manith Luthria <mnl21@duke.edu>
2022-12-13 00:53:53 -0500

    changed viewinfo to have margins
    Former-commit-id: 9acc1ff3b9a1d8ba5c2d568d8e40582ce2a97d40

--- src/main/java/ooga/view/ViewInfo.java
+++ src/main/java/ooga/view/ViewInfo.java
@@ -1,4 +1,4 @@
 package ooga.view;
 
-public record ViewInfo(String name, String backgroundURL, int cameraHeight, int cameraWidth, String style, String scrollingDirection) {
+public record ViewInfo(String name, String backgroundURL, int marginRight, int marginLeft, int marginTop, int marginBottom) {
 }
