9029d50b562f9ead981b32a4ad364eed59527aad
Ryan Wolfram <ryan.wolfram@duke.edu>
2023-01-13 18:20:32 -0500

    delete doc directory

--- doc/FOLDER_PURPOSE.md
+++
@@ -1,3 +0,0 @@
-# doc
-
-Put any written documents related to your project here, including relevant lab discussions.
--- doc/plan/api/model/collision_handling/CollisionChartLoader.java
+++
@@ -1,6 +0,0 @@
-/**
- * Loads the CollisionChart object from a JSON file for entityType passed as a parameter.
- */
-public interface CollisionChartLoader {
-  CollisionChart loadCollisionChart(String entityType);
-}
